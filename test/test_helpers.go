305baf78744c1f851186cabbc914f80bd5a0e980
Court Fowler <courtf@gmail.com>
2014-04-22 14:59:50 -0700

    better test helpers

--- test/test_helpers.go
+++ test/test_helpers.go
@@ -42,18 +42,30 @@ func IsNillable(k reflect.Kind) (nillable bool) {
 	return
 }
 
+// Warning: directly comparing functions is unreliable
 func Expect(t *testing.T, a interface{}, b interface{}) {
+	btype := reflect.TypeOf(b)
 	if b == nil {
 		MustBeNil(t, a)
-	} else if a != b {
+	} else if btype.Kind() == reflect.Func {
+		if reflect.ValueOf(a).Pointer() != reflect.ValueOf(b).Pointer() {
+			t.Errorf("Expected func %v (type %v) to equal func %v (type %v).", b, reflect.TypeOf(b), a, reflect.TypeOf(a))
+		}
+	} else if !reflect.DeepEqual(a, b) {
 		t.Errorf("Expected %v (type %v) - Got %v (type %v)", b, reflect.TypeOf(b), a, reflect.TypeOf(a))
 	}
 }
 
+// Warning: directly comparing functions is unreliable
 func Refute(t *testing.T, a interface{}, b interface{}) {
+	btype := reflect.TypeOf(b)
 	if b == nil {
 		MustNotBeNil(t, a)
-	} else if a == b {
+	} else if btype.Kind() == reflect.Func {
+		if reflect.ValueOf(a).Pointer() == reflect.ValueOf(b).Pointer() {
+			t.Errorf("Expected func %v (type %v) to not equal func %v (type %v).", b, reflect.TypeOf(b), a, reflect.TypeOf(a))
+		}
+	} else if reflect.DeepEqual(a, b) {
 		t.Errorf("Did not expect %v (type %v) - Got %v (type %v)", b, reflect.TypeOf(b), a, reflect.TypeOf(a))
 	}
 }
