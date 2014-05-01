500172607359051599a08722f7c23b788f37224d
Josh Yaganeh <jyaganeh@gmail.com>
2014-05-01 13:19:53 -0700

    removed gorilla/mux in favor of net/http

--- handlers.go
+++ handlers.go
@@ -10,41 +10,33 @@ import (
 	"time"
 
 	"github.com/geoloqi/geobin-go/socket"
-	"github.com/gorilla/mux"
 	"github.com/nu7hatch/gouuid"
 	redis "github.com/vmihailenco/redis/v2"
 )
 
-// Creates a new router, with all handlers set up
-func createRouter() *mux.Router {
-	r := mux.NewRouter()
-	// API routes (POSTs only!)
-	api := r.Methods("POST").PathPrefix("/api/{v:[0-9.]+}/").Subrouter()
-	api.HandleFunc("/create", createHandler)
-	api.HandleFunc("/history/{name}", historyHandler)
-	api.HandleFunc("/ws/{name}", wsHandler)
+func createRouter() *http.ServeMux {
+	r := http.NewServeMux()
 
-	// Our bread and/or butter (how requests actually get put into redis)
-	r.HandleFunc("/{name}", binHandler).Methods("POST")
-
-	// Client/web requests (GETs only!)
-	web := r.Methods("GET").Subrouter()
-	// Any GET request to the /api/ route will serve up the docs static site directly.
-	web.PathPrefix("/api").HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
-		debugLog("docs -", req.URL)
-		// TODO: This is wrong, will fix when we actually have the files to serve
-		http.ServeFile(w, req, "docs/build/")
+	// Web routes
+	r.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
+		switch req.Method {
+		case "GET":
+			debugLog("web -", req.URL)
+			http.ServeFile(w, req, "static/app/index.html")
+		case "POST":
+			binHandler(w, req)
+		}
 	})
-	// Any GET request to the /static/ route will serve the files in the static dir directly.
-	web.PathPrefix("/static/").HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+	r.HandleFunc("/static/", func(w http.ResponseWriter, req *http.Request) {
 		debugLog("static -", req.URL)
 		http.ServeFile(w, req, req.URL.Path[1:])
 	})
-	// All other GET requests will serve up the Angular app at static/index.html
-	web.PathPrefix("/").HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
-		debugLog("web -", req.URL)
-		http.ServeFile(w, req, "static/app/index.html")
-	})
+
+	// API routes
+	r.HandleFunc("/api/1/create", createHandler)
+	r.HandleFunc("/api/1/history/", historyHandler) // /api/1/history/{bin_id}
+	r.HandleFunc("/api/1/ws/", wsHandler)           // api/1/ws/{bin_id}
+
 	return r
 }
 
@@ -95,7 +87,7 @@ func createHandler(w http.ResponseWriter, r *http.Request) {
 // log a request into a bin
 func binHandler(w http.ResponseWriter, r *http.Request) {
 	debugLog("bin -", r.URL)
-	name := mux.Vars(r)["name"]
+	name := r.URL.Path[1:]
 
 	exists, err := nameExists(name)
 	if err != nil {
@@ -140,7 +132,9 @@ func binHandler(w http.ResponseWriter, r *http.Request) {
 // Get bin history
 func historyHandler(w http.ResponseWriter, r *http.Request) {
 	debugLog("history -", r.URL)
-	name := mux.Vars(r)["name"]
+	path := strings.Split(r.URL.Path, "/")
+	name := path[len(path)-1]
+
 	exists, err := nameExists(name)
 	if err != nil {
 		http.Error(w, "Internal error", http.StatusInternalServerError)
@@ -181,8 +175,8 @@ func historyHandler(w http.ResponseWriter, r *http.Request) {
 // Web socket connections
 func wsHandler(w http.ResponseWriter, r *http.Request) {
 	debugLog("create -", r.URL)
-	// upgrade the connection
-	binName := mux.Vars(r)["name"]
+	path := strings.Split(r.URL.Path, "/")
+	binName := path[len(path)-1]
 
 	// start pub subbing
 	if err := pubsub.Subscribe(binName); err != nil {
